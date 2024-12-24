7a68a4c094c35435535f123d5fb7b5e69d9d40f0
123soikot <162465476+123soikot@users.noreply.github.com>
2024-12-24 15:52:35 +0600

    Update Gemini2.js

--- TINA/commands/Gemini2.js
+++ TINA/commands/Gemini2.js
@@ -1,7 +1,8 @@
 const axios = require("axios");
+
 const baseApiUrl = async () => {
   const base = await axios.get(
-    `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`,
+    `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`
   );
   return base.data.api;
 };
@@ -22,40 +23,39 @@ module.exports = {
   },
   run: async ({ api, args, event }) => {
     const prompt = args.join(" ");
-    //---- Image Reply -----//
+
     if (event.type === "message_reply") {
       var t = event.messageReply.attachments[0].url;
+
       try {
         const response = await axios.get(
-          `${await baseApiUrl()}/gemini?prompt=${encodeURIComponent(prompt)}&url=${encodeURIComponent(t)}`,
+          `${await baseApiUrl()}/gemini?prompt=${encodeURIComponent(prompt)}&url=${encodeURIComponent(t)}`
         );
         const data2 = response.data.dipto;
         api.sendMessage(data2, event.threadID, event.messageID);
       } catch (error) {
         console.error("Error:", error.message);
-        api.sendMessage(error, event.threadID, event.messageID);
+        api.sendMessage(error.message, event.threadID, event.messageID);
       }
-    }
-    //---------- Message Reply ---------//
-    else if (!prompt) {
+    } else if (!prompt) {
       return api.sendMessage(
         "Please provide a prompt or message reply",
         event.threadID,
-        event.messageID,
+        event.messageID
       );
     } else {
       try {
-        const respons = await axios.get(
-          `${await baseApiUrl()}/gemini?prompt=${encodeURIComponent(prompt)}`,
+        const response = await axios.get(
+          `${await baseApiUrl()}/gemini?prompt=${encodeURIComponent(prompt)}`
         );
-        const message = respons.data.dipto;
+        const message = response.data.dipto;
         api.sendMessage(message, event.threadID, event.messageID);
       } catch (error) {
-        console.error("Error calling Gemini AI:", error);
+        console.error("Error calling Gemini AI:", error.message);
         api.sendMessage(
-          `Sorry, there was an error processing your request.${error}`,
+          `Sorry, there was an error processing your request: ${error.message}`,
           event.threadID,
-          event.messageID,
+          event.messageID
         );
       }
     }
